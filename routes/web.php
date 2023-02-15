564a9e60cea539010a9444149299c194db629ccf
Stephan DuVal <stephan.duval@gmail.com>
2023-02-14 21:51:55 -0800

    created an example card

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/example', function () {
+    return view('example');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
